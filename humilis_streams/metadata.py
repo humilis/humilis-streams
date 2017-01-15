d946859e4f8f28bd21d64c801caca7dfe2968cf0
German Gomez-Herrero <german@innovativetravel.eu>
2017-01-15 22:48:57 +0100

    Add Cloudwatch alarms

--- humilis_streams/metadata.py
+++ humilis_streams/metadata.py
@@ -1,13 +1,12 @@
-# -*- coding: utf-8 -*-
 """Project metadata."""
 
 package = "humilis_streams"
 project = "humilis-streams"
-version = '0.0.4'
-description = "Humilis layer that deploys Kinesis streams"
+version = '0.0.5'
+description = "Deploys Kinesis streams"
 authors = ["German Gomez-Herrero"]
 authors_string = ', '.join(authors)
-emails = ["german@innovativetravel.eu"]
+emails = ["german@findhotel.net"]
 license = "MIT"
-copyright = "2016 Innovative Travel Ltd"
-url = 'http://github.com/InnovativeTravel/humilis-streams'
+copyright = "2017, German Gomez-Herrero and FindHotel BV"
+url = 'http://github.com/humilis/humilis-streams'
