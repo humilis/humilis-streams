325d3b2b65e55eb4ccab27557bba34b866656cdc
German Gomez-Herrero <german@innovativetravel.eu>
2017-07-29 10:26:55 +0300

    Support for custom retention periods

--- humilis_streams/metadata.py
+++ humilis_streams/metadata.py
@@ -2,7 +2,7 @@
 
 package = "humilis_streams"
 project = "humilis-streams"
-version = '0.0.6'
+version = '0.0.7'
 description = "Deploys Kinesis streams"
 authors = ["German Gomez-Herrero"]
 authors_string = ', '.join(authors)
